dff108e06113d66ba8b36afed5e0b5f875445ab5
leedongyeop <ldy_1204@naver.com>
2023-05-29 20:40:56 +0900

    test: 스프링 부트 테스트로 전환하기

--- src/test/java/com/example/productorderservice/product/ProductServiceTest.java
+++ src/test/java/com/example/productorderservice/product/ProductServiceTest.java
@@ -1,47 +1,29 @@
 package com.example.productorderservice.product;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
+@SpringBootTest
 class ProductServiceTest {
 
+    @Autowired
     private ProductService productService;
-    private StubProductPort productPort = new StubProductPort();
-
-    @BeforeEach
-    void setUp() {
-        productService = new ProductService(productPort);
-    }
 
     @Test
     public void 상품수정() throws Exception {
 
+        productService.addProduct(ProductSteps.상품등록요청_생성());
+
         final Long productId = 1L;
         final UpdateProductRequest request = new UpdateProductRequest("상품 수정", 2000, DiscountPolicy.NONE);
-        final Product product = new Product("상품명", 1000, DiscountPolicy.NONE);
 
-        productPort.getProduct_will_return = product;
         productService.updateProduct(productId, request);
 
-        Assertions.assertThat(product.getName()).isEqualTo("상품 수정");
-    }
-
-    private static class StubProductPort implements ProductPort {
-
-        public Product getProduct_will_return;
-
-        @Override
-        public void save(Product product) {
-
-        }
-
-        @Override
-        public Product getProduct(Long productId) {
-            return getProduct_will_return;
-        }
+        final ResponseEntity<GetProductResponse> response = productService.getProduct(productId);
+        final GetProductResponse productResponse = response.getBody();
+        Assertions.assertThat(productResponse.name()).isEqualTo("상품 수정");
     }
 }
