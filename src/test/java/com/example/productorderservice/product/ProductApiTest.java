e10dba0b8bdf81b3ce56c6b627f828050c864af0
leedongyeop <ldy_1204@naver.com>
2023-05-29 17:50:01 +0900

    test: API test로 전환하기

--- src/test/java/com/example/productorderservice/product/ProductApiTest.java
+++ src/test/java/com/example/productorderservice/product/ProductApiTest.java
@@ -1,6 +1,9 @@
 package com.example.productorderservice.product;
 
 import com.example.productorderservice.ApiTest;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
@@ -18,5 +21,24 @@ class ProductApiTest extends ApiTest {
         final var response = 상품등록요청(request);
 
         Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+
+    @Test
+    public void 상품조회() throws Exception {
+        //상품 등록
+        상품등록요청(상품등록요청_생성());
+
+        //등록한 상품의 번호
+        final Long productId = 1L;
+
+        final var response = RestAssured.given().log().all()
+                .when()
+                .get("/products/{productId}", productId)
+                .then().log().all()
+                .extract();
+
+        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        Assertions.assertThat(response.jsonPath().getString("name")).isEqualTo("상품명");
     }
 }
