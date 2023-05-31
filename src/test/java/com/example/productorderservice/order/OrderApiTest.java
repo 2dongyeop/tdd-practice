74d6d93e0c05c1927aca2cba9028d94fa539612e
leedongyeop <ldy_1204@naver.com>
2023-05-31 18:46:01 +0900

    feat: JPA 적용

--- src/test/java/com/example/productorderservice/order/OrderApiTest.java
+++ src/test/java/com/example/productorderservice/order/OrderApiTest.java
@@ -3,10 +3,14 @@ package com.example.productorderservice.order;
 import com.example.productorderservice.ApiTest;
 import com.example.productorderservice.product.ProductSteps;
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
+import static com.example.productorderservice.order.OrderSteps.상품주문요청;
+import static com.example.productorderservice.order.OrderSteps.상품주문요청_생성;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class OrderApiTest extends ApiTest {
@@ -15,23 +19,10 @@ class OrderApiTest extends ApiTest {
     public void 상품주문() throws Exception {
 
         ProductSteps.상품등록요청(ProductSteps.상품등록요청_생성());
-        final CreateOrderRequest request = 상품주문요청_생성();
+        final var request = 상품주문요청_생성();
 
-        final var response = RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(request)
-                .when()
-                .post("/orders")
-                .then()
-                .log().all().extract();
+        final var response = 상품주문요청(request);
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-    }
-
-    private static CreateOrderRequest 상품주문요청_생성() {
-        final Long productId = 1L;
-        final int quantity = 2;
-        final CreateOrderRequest request = new CreateOrderRequest(productId, quantity);
-        return request;
     }
 }
